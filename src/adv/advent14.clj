82d92c2d3b5ab3ea033383c49568c75ccbe3eba0
Mike Zamansky <zamansky@gmail.com>
2016-01-03 17:57:29 -0500

    finished 14 part 1

---
+++ src/adv/advent14.clj
@@ -0,0 +1,33 @@
+(require '[clojure.string :as String])
+(defn parse-int [s] (Integer/parseInt s))
+
+(def input (String/split  (slurp "advent14.txt") #"\n"))
+
+(def splitter #"([A-Z][a-z]+) [a-z \/]+([0-9]+) [a-z \/]+([0-9]+) [a-z, ]+ ([0-9]+)")
+
+
+(defn intify [x]
+  "convert ints from strinnt to int in list"
+  (map #(if (re-find #"\d+" %) (parse-int %)% ) x))
+
+
+(def lines
+  (->> input
+       (map #(drop 1 (re-find splitter %)))
+       (map  intify)
+       ))
+
+(def totaltime 2503)
+(defn dist
+  [ [name spd time rest]]
+  (let [
+        base (* (quot totaltime (+ time rest))
+                (* spd time))
+        resttime (mod totaltime (+ time rest))
+        addontime (min resttime time)
+        ]
+    [name  (+ base (* addontime spd))] )
+  )
+
+
+(map dist lines)
