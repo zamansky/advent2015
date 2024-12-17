1e0430b851971bdb533eaa81733bedd479e7213d
Mike Zamansky <zamansky@gmail.com>
2024-12-16 21:01:20 -0500

    finished 17 part 1

---
+++ src/day17.clj
@@ -0,0 +1,38 @@
+(ns day17
+    (:require
+   [utils :as u]
+   [hashp.core]
+   [clojure.string :as str]
+   [clojure.java.io :as io]
+   [clojure.edn :as edn]
+   [clojure.set :as set]
+   [clojure.math :as m]
+   [clojure.math.numeric-tower :as math :refer [expt abs]]
+   [clojure.math.combinatorics :as comb]
+   ))
+
+;; (u/load-data 2015 17)
+
+(def data (slurp "data/day17.dat"))
+
+(def sample [20 15 10 5 5])
+
+(def data [11 30 47 31 32 36 3 1 5 3 32 36 15 11 46 26 28 1 19 3])
+  
+(defn part1 [containers target]
+  (cond (= target 0)
+        1
+
+        
+        (empty? containers)
+        0
+
+        :else 
+        (+
+         (part1 (rest containers) (- target (first containers)))
+         (part1 (rest containers) target))))
+
+(part1 sample 25)
+(part1 data 150)
+
+
